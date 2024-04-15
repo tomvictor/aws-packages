a03a9dccdca917e9c491e8af2f8f62e69fc8e890
Tom Victor <vjtomvictor@icloud.com>
2024-04-15 23:09:28 +0200

    add error msg response

--- aws_packages/responses.py
+++ aws_packages/responses.py
@@ -18,3 +18,11 @@ def success_response(body, status_code=HTTPStatus.OK.value):
         content_type=content_types.APPLICATION_JSON,
         body=body,
     )
+
+
+def error_message(msg="error", data=None, status_code=HTTPStatus.BAD_REQUEST.value):
+    return Response(
+        status_code=status_code,
+        content_type=content_types.APPLICATION_JSON,
+        body={"message": msg, "data": data},
+    )
